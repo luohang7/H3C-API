34638f81febdb9caa03fd0a38c6e41a4f03b4425
luohang <luo3405@gmail.com>
2024-07-25 17:41:22 +0800

    ok

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,7 @@ from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 process = None  # 用于跟踪当前运行的进程
+socketio = SocketIO(app)  # 确保这里正确初始化
 
 # 设置日志
 logging.basicConfig(level=logging.DEBUG)
@@ -19,9 +20,8 @@ if not os.path.exists(UPLOAD_FOLDER):
 def run_script():
     global process
     script = request.json.get('script')
-    app.logger.debug(f'Received script request: {script}')
     python_interpreter = sys.executable
-    app.logger.debug(f'Using Python interpreter: {python_interpreter}')
+
     try:
         if script == 'check_version':
             process = subprocess.Popen([python_interpreter, 'check_version.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, encoding='utf-8')
@@ -30,28 +30,29 @@ def run_script():
         else:
             return jsonify({"error": "错误的脚本名称"}), 400
 
-        app.logger.info(f'Started script with PID: {process.pid}')
-        stdout, stderr = process.communicate()
-        process = None
-        return jsonify({"output": stdout, "error": stderr})
+        socketio.start_background_task(target=stream_output, process=process)
+        'process = None'
+        return jsonify({"message": "脚本启动成功"})
     except Exception as e:
-        app.logger.error(f'Error running script: {e}')
         process = None
         return jsonify({"错误": str(e)}), 500
+
+def stream_output(process):
+    for line in process.stdout:
+        socketio.emit('脚本输出', {'output': line})
+    process.stdout.close()
+    process.wait()
+    socketio.emit('脚本输出', {'output': '运行完毕'})
 
 @app.route('/stop_script', methods=['POST'])
 def stop_script():
     global process  # 声明为全局变量
-    app.logger.info('Received stop script request')
     if process:
-        app.logger.info('Stopping the script...')
         process.terminate()  # 终止进程
         process.wait()
         process = None
-        app.logger.info('Script stopped successfully')
         return jsonify({"message": "脚本已停止"}), 200
     else:
-        app.logger.error('No script is running')
         return jsonify({"error": "脚本未运行"}), 400
 
 @app.route('/upload_csv', methods=['POST'])
