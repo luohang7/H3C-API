7006753dcf582c577ca632ebaa3f663752913f2f
luohang <luoh3405@gmail.com>
2024-07-28 16:50:52 +0800

    ok

---
+++ frontend/src/components/DeviceUpgrade.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <h1>设备升级</h1>
+    <!-- 脚本选择器组件 -->
+    <script-selector />
+    <!-- 文件上传组件 -->
+    <file-upload />
+  </v-container>
+</template>
+
+<script>
+import ScriptSelector from './ScriptSelector.vue';
+import FileUpload from './FileUpload.vue';
+
+export default {
+  name: 'DeviceUpgrade',
+  components: {
+    ScriptSelector,
+    FileUpload,
+  },
+};
+</script>
+
+<style>
+@import "~vuetify/dist/vuetify.min.css";
+</style>
