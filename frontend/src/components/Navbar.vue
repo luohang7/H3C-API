90d983c6e7ad48ab50c1f03de00da508b8ce2093
luohang <luoh3405@gmail.com>
2024-07-28 17:21:57 +0800

    ok

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <v-toolbar-title>设备管理系统</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn text @click="navigateToUpgrade">设备升级</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  methods: {
+    navigateToUpgrade() {
+      if (this.$route.path !== '/') {
+        this.$router.push('/');
+      }
+    }
+  }
+}
+</script>
